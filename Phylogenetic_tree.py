70fbfcb8268b16a3b05d52ad464fd202671d1773
BrendanSchroyen <147394002+BrendanSchroyen@users.noreply.github.com>
2023-10-10 13:48:37 +0200

    Update Phylogenetic_tree.py

--- Phylogenetic_tree.py
+++ Phylogenetic_tree.py
@@ -25,17 +25,33 @@ for node in tree.get_nonterminals():
 # Save the tree as a Newick file
 Phylo.write(tree, "YourTree.nwk", "newick")
 
-tree_file = "YourTree.nwk"
+from ete3 import Tree, TreeStyle, NodeStyle
 
-# Load the tree from the Newick file
-tree = Phylo.read(tree_file, "newick")
+# Load the tree from a Newick file
+tree = Tree("YourTree.nwk", quoted_node_names=True)
 
-# Customize the font size
-plt.rcParams['font.size'] = 35
+# Create a TreeStyle object for customization
+ts = TreeStyle()
+ts.mode = "c"  # Circular layout
+ts.arc_start = -90
+ts.arc_span = 180
+ts.branch_vertical_margin = 1
 
-# Draw the tree
-fig, ax = plt.subplots(figsize=(120, 100))
-ax.set_facecolor('white')  # Set the background color to white
-Phylo.draw(tree, axes=ax, do_show=False)
+# Customize the branch color based on leaf names
+for node in tree.traverse():
+    if node.is_leaf() and 'Class_II' in node.name:
+        node_color = "red"
+        # Color the branches leading to the leaf node
+        for ancestor in node.iter_ancestors():
+            ancestor_style = NodeStyle()
+            #ancestor_style["bgcolor"] = node_color
+            #ancestor_style["hz_line_color"] = node_color
+            #ancestor_style["fgcolor"] = node_color
+            ancestor.set_style(ancestor_style)
 
-plt.savefig('YourTree.png')
+# Additional customization options (optional)
+ts.show_leaf_name = True
+ts.scale = 600  # Controls the size of the tree
+
+# Apply the TreeStyle to the tree and render it
+tree.render("YourTree.svg", tree_style=ts)
